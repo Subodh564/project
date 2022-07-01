650d09581bcff0629d896e8b8d7091d8eec12014
Subodh Shah <Subodhshah564@gmail.com>
2022-07-01 09:45:28 +0545

    added implementation for dif.java

--- dif.java
+++ dif.java
@@ -1,5 +1,7 @@
 public class dif {
-    public static void main(String [] args){\
+    public static void main(String [] args){
+        int a=20,b=30;
+        System.out.println(a+b);
     }
     
 }
